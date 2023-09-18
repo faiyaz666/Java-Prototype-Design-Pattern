c8cb195229eff0efa0ba2eb23406ad205bd13bde
faiyaz666 <faiyazrafid5@gmail.com>
2023-09-18 12:55:59 +0600

    Initial Commit

--- src/main/java/com/DesignPattern/MainClass.java
+++ src/main/java/com/DesignPattern/MainClass.java
@@ -1,8 +1,19 @@
 package com.DesignPattern;
 
+import java.util.List;
+
 public class MainClass {
     public static void main(String[] args) {
         StudentClass students = new StudentClass();
+        students.setStudentList("Rafid");
+
+        printStudent(students.getStudentList());
 
     }
+    private static void printStudent(List<String> student){
+        for (String name: student){
+            System.out.println(name);
+        }
+    }
+
 }
--- src/main/java/com/DesignPattern/StudentClass.java
+++ src/main/java/com/DesignPattern/StudentClass.java
@@ -5,5 +5,31 @@ import java.util.List;
 
 public class StudentClass {
 
-    private List<String> StudentList = new ArrayList<>();
+    private List<String> StudentList;
+
+    public StudentClass(){
+        System.out.println("Default Constructor");
+        loadDatabase();
+    }
+
+    public StudentClass(List<String> studentList) {
+        System.out.println("Custom Constructor");
+        this.StudentList = studentList;
+    }
+
+    public List<String> getStudentList() {
+        return StudentList;
+    }
+
+    public void setStudentList(String name) {
+        StudentList.add(name);
+    }
+
+    public void loadDatabase(){
+        System.out.println("Data loaded from database");
+        StudentList = new ArrayList<>();
+        StudentList.add("Atique");
+        StudentList.add("Rashmi");
+        StudentList.add("Ishan");
+    }
 }
